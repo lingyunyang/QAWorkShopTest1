4cccedb3c0b5c01e5742b011705452e296ccb2d4
gszhangwei <279230581@qq.com>
2017-09-20 20:02:45 +0800

    test coverage

--- webdemo/src/test/java/org/gradle/demo/HelloServletTest.java
+++ webdemo/src/test/java/org/gradle/demo/HelloServletTest.java
@@ -24,17 +24,17 @@ public class HelloServletTest {
         MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    public void doGet() throws Exception {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);
-
-        when(response.getWriter()).thenReturn(printWriter);
-
-        new HelloServlet().doGet(request, response);
-
-        assertEquals("Hello, World!", stringWriter.toString());
-    }
+//    @Test
+//    public void doGet() throws Exception {
+//        StringWriter stringWriter = new StringWriter();
+//        PrintWriter printWriter = new PrintWriter(stringWriter);
+//
+//        when(response.getWriter()).thenReturn(printWriter);
+//
+//        new HelloServlet().doGet(request, response);
+//
+//        assertEquals("Hello, World!", stringWriter.toString());
+//    }
 
     @Test
     public void doPostWithoutName() throws Exception {
