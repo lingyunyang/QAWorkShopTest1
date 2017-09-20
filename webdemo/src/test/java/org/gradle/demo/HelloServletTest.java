80afe4937c873338061ea7b531ac389b91e53e7a
gszhangwei <279230581@qq.com>
2017-09-20 20:26:59 +0800

    Test coverage degrade

--- webdemo/src/test/java/org/gradle/demo/HelloServletTest.java
+++ webdemo/src/test/java/org/gradle/demo/HelloServletTest.java
@@ -14,15 +14,15 @@ import java.io.StringWriter;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
-//public class HelloServletTest {
-//    @Mock private HttpServletRequest request;
-//    @Mock private HttpServletResponse response;
-//    @Mock private RequestDispatcher requestDispatcher;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        MockitoAnnotations.initMocks(this);
-//    }
+public class HelloServletTest {
+    @Mock private HttpServletRequest request;
+    @Mock private HttpServletResponse response;
+    @Mock private RequestDispatcher requestDispatcher;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
 
 //    @Test
 //    public void doGet() throws Exception {
@@ -46,16 +46,16 @@ import static org.mockito.Mockito.*;
 //        verify(request).setAttribute("user", "World");
 //        verify(requestDispatcher).forward(request,response);
 //    }
-//
-//    @Test
-//    public void doPostWithName() throws Exception {
-//        when(request.getParameter("name")).thenReturn("Dolly");
-//        when(request.getRequestDispatcher("response.jsp"))
-//                .thenReturn(requestDispatcher);
-//
-//        new HelloServlet().doPost(request, response);
-//
-//        verify(request).setAttribute("user", "Dolly");
-//        verify(requestDispatcher).forward(request,response);
-//    }
-//}
+
+    @Test
+    public void doPostWithName() throws Exception {
+        when(request.getParameter("name")).thenReturn("Dolly");
+        when(request.getRequestDispatcher("response.jsp"))
+                .thenReturn(requestDispatcher);
+
+        new HelloServlet().doPost(request, response);
+
+        verify(request).setAttribute("user", "Dolly");
+        verify(requestDispatcher).forward(request,response);
+    }
+}
